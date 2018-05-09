518c827d7bb8cb10e29c37fc5000ca794d2123af
zhaihaigang <zhaihaigang@vip.house365.com>
2018-05-09 10:16:50 +0800

    Initial Commit

---
+++ QYStarDisplay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QYStarDisplay
+//
+//  Created by qiannian on 2018/5/9.
+//  Copyright © 2018年 qiannian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QYStarDisplay/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  QYStarDisplay
+//
+//  Created by qiannian on 2018/5/9.
+//  Copyright © 2018年 qiannian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
